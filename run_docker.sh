9d2771a88fd32023172e7c55ef61a72552934e71
Tuomas Jaakola <tuomas.jaakola@iki.fi>
2022-03-29 12:29:35 +0300

    Docker: do not use -it flags, can be run by anacron now

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-docker run --rm -it -e TZ="Europe/Helsinki" -v $PWD:/code sheet-to-cal
+
+# Runs the app in Docker container.
+# You can start this script by anacron.
+# Note that stdout and stderr is redirected into a log file.
+
+LOGFILE="/tmp/sheet-to-cal.log"
+docker run --rm -e TZ="Europe/Helsinki" -v $PWD:/code sheet-to-cal > $LOGFILE 2>&1
 if [ $? -ne 0 ]; then
   echo "Make sure you have built Docker image:"
   echo "docker build -t sheet-to-cal ."
